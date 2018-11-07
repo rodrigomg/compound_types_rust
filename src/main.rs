1fec3cb66fde47f2e74352b0ab8b4dff45d22eba
technosoul <rockdrigo.mtz@gmail.com>
2018-11-07 13:26:45 -0600

    pattern matching for tuple :boom:

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 fn main() {
     tuple_sample();
+    tuple_destructure();
 }
 
 fn tuple_sample(){
     let tup: (i32, f64, u8) = (500, 6.4, 1);
     println!("Tuple: {:?}", tup);
+}
+
+fn tuple_destructure(){
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+    println!("The value of z is: {}", z);
 }
