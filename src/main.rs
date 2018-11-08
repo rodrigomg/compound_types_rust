091a9fe6056b5f7d28fac7b83f7339e0fea0b1ba
technosoul <rockdrigo.mtz@gmail.com>
2018-11-07 18:43:49 -0600

    destructure tuple by index :zap:

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     tuple_sample();
     tuple_destructure();
+    tuple_destructure_by_index();
 }
 
 fn tuple_sample(){
@@ -14,4 +15,14 @@ fn tuple_destructure(){
     println!("The value of x is: {}", x);
     println!("The value of y is: {}", y);
     println!("The value of z is: {}", z);
+}
+
+fn tuple_destructure_by_index(){
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let one = x.0;
+    let two = x.1;
+    let last = x.2;
+    println!("The value of one is: {}", one);
+    println!("The value of two is: {}", two);
+    println!("The value of last is: {}", last);
 }
